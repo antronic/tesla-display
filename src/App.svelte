7df44f8d7b76b6dac89e1e25f0db79d7523bf7cf
Jirachai Chansivanon <jirachai.c@outlook.com>
2024-04-11 10:28:56 +0700

    ✨ Initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,22 @@
 
   const generateRedirectUrl = (url: string) => `https://www.youtube.com/redirect?q=${url}`
 
+  const quickAccess = window.localStorage.getItem('ITESLA_DISPLAY_QUICKACCESS')
+  const savedQuickAccess = quickAccess ? JSON.parse(quickAccess) : []
+
   const preDefinedLinks = [
     { name: 'Bilibili', href: 'https://www.bilibili.tv' },
+    ...savedQuickAccess,
   ]
 
   const links = preDefinedLinks.map(({ name, href }) => ({ name, href: generateRedirectUrl(href) }))
 
-  function onShareClick() {
-    navigator.share({ url: generateRedirectUrl(urlToShareInput) })
+  function onShareClick(fullscreen?: boolean) {
+    navigator.share({ url: fullscreen ? generateRedirectUrl(urlToShareInput) : urlToShareInput })
+  }
+
+  function goWithRedirect(url: string) {
+    window.location.replace(generateRedirectUrl(url))
   }
 </script>
 
@@ -24,8 +32,10 @@
 
   <!-- Quick access -->
   <div class="px-8 mt-16">
-    <div class="text-left my-4">
+    <div class="text-left my-4 flex items-center gap-x-2">
       <h3 class="">Quick access</h3>
+
+      <!-- <Icon icon="ooui:edit" class="text-xl"/> -->
     </div>
 
     <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 sm:grid-cols-6 gap-x-4">
@@ -46,14 +56,22 @@
     <div class="grid grid-cols-12 gap-x-4">
       <!-- Input and button in same row -->
       <div class="col-span-10">
-        <input on:input={(e) => { urlToShareInput = e.target.value }} type="text" class="w-full p-2 border border-gray-300 rounded-md" value=""/>
+        <input on:input={(e) => { urlToShareInput = e.target.value }} type="text" class="w-full p-2 border border-gray-300 rounded-md text-xl" value=""/>
       </div>
       <div class="col-span-2">
-        <button class="w-full p-2 bg-blue-500 text-white rounded-md text-center flex justify-center" on:click={onShareClick}>
+        <!-- <button class="w-full p-2 bg-blue-500 text-white rounded-md text-center flex justify-center" on:click={() => onShareClick()}>
           <Icon icon="ooui:share" class="text-2xl"/>
+        </button> -->
+        <button class="w-full p-2 bg-blue-500 text-white rounded-md text-center flex justify-center" on:click={() => goWithRedirect(urlToShareInput)}>
+          <Icon icon="ooui:full-screen" class="text-2xl"/>
         </button>
       </div>
     </div>
   </div>
+
+  <!-- History share -->
+  <!-- Login -->
+  <!-- Share list -->
+
 
 </main>
--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
   import Icon from '@iconify/svelte'
+
+  function addNewQuickAccess() {
+    console.log('Add new quick access')
+    const url = prompt('Enter the URL:')
+    if (!url) return
+
+    let name = prompt('Enter the name of the quick access') || url
+
+    const currentData = window.localStorage.getItem('ITESLA_DISPLAY_QUICKACCESS')
+    const json = JSON.parse(currentData || '[]')
+    window.localStorage.setItem('ITESLA_DISPLAY_QUICKACCESS', JSON.stringify([...json, { name, href: url }]))
+  }
 </script>
 
 <nav class="px-8 py-4 fixed top-0 left-0 w-full bg-zinc-800">
@@ -9,7 +21,7 @@
       <p class="text-white">Tesla dashboard</p>
     </div>
     <div class="flex justify-end text-blue-500 text-xl">
-      <button class="">
+      <button class="" on:click={addNewQuickAccess}>
         <Icon icon="ooui:add" class=""/>
       </button>
     </div>
